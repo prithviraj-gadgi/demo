e7526a4e8eff9be483843aceb903e48299338d47
Prithviraj Gadgi <prithvirajgadgi@gmail.com>
2024-09-24 21:35:21 +0530

    Testing spotless plugin

---
+++ src/main/java/com/demo/dto/TestSpotless.java
@@ -0,0 +1,9 @@
+package com.demo.dto;
+
+import java.time.LocalDateTime;
+
+public class TestSpotless
+{
+            private String message;
+    private int         statusCode;
+                    private LocalDateTime timestamp;}
